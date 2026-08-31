1d377539760200b27e697f4aaffa7da5ecfbd1eb
QuentinHamonic <56317359+QuentinHamonic@users.noreply.github.com>
2026-08-31 11:04:18 +0200

    TP02 service

--- src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -2,15 +2,24 @@ package fr.diginamic.hello.controleurs;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.diginamic.hello.services.HelloService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
 
+    private HelloService helloService;
+
+    public HelloControleur(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping
     public String direHello() {
-        return "Hello";
+        return helloService.salutations();
     }
 
 }
